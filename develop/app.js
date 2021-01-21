3fc51b053882386ed183f8263612ca96c6d5f759
Sim kaur <moblue2012@gmail.com>
2021-01-21 18:47:04 -0500

    final version

--- develop/app.js
+++ develop/app.js
@@ -14,140 +14,173 @@ const employeeArr = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function createManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What's the manager name?"
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What's the manager ID?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What's the manager Email?"
+            },
+            {
+                type: "input",
+                name: "managerOffice",
+                message: "What's the manager office number?"
+            },
+        ])
+        .then(answers => {
+            // Use user feedback for... whatever!!
+            console.log(answers)
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice)
+            console.log("This is the new manager" + JSON.stringify(manager))
 
-inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "managerName",
-            message: "What's the manager name?"
-        },
-        {
-            type: "input",
-            name: "managerId",
-            message: "What's the manager ID?"
-        },
-        {
-            type: "input",
-            name: "managerEmail",
-            message: "What's the manager Email?"
-        },
-        {
-            type: "input",
-            name: "managerOffice",
-            message: "What's the manager office number?"
-        },
-    ])
-    .then(answers => {
-        // Use user feedback for... whatever!!
-        console.log(answers)
-        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice)
-        console.log("This is the new manager" + JSON.stringify(manager))
+            employeeArr.push(manager)
 
-        employeeArr.push(manager)
-    })
-    .catch(error => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            
-            // Something else when wrong
-        }
-    });
+            promptForTeamMember()
 
+        })
+        .catch(error => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
 
+                // Something else when wrong
+            }
+        });
+}
 
+function promptForTeamMember() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "member_type",
+                message: "which type of member would you like to add ?",
+                choices: ["Engineer", "Intern","Done"]
+            },
+        ])
+        .then(answers => {
+            console.log(answers)
+            if (answers.member_type === "Intern")
+                createIntern ()
 
+            else if (answers.member_type === "Engineer")
+                createEngineer ()
+
+            else createTeam ()
+
+        });
+}
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "engineerName",
-            message: "What's the engineer name?"
-        },
-        {
-            type: "input",
-            name: "engineerId",
-            message: "What's the engineer ID?"
-        },
-        {
-            type: "input",
-            name: "engineerEmail",
-            message: "What's the engineer Email?"
-        },
-        {
-            type: "input",
-            name: "engineerGithub",
-            message: "What's the engineer Github?"
-        },
-    ])
-    .then(answers => {
-        // Use user feedback for... whatever!!
-        console.log(answers)
-        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
-        console.log("This is the new engineer" + JSON.stringify(engineer))
-
-        employeeArr.push(engineer)
-    })
-    .catch(error => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            
-            // Something else when wrong
-        }
-    });
-    
+function createEngineer() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "internName",
-            message: "What's the intern name?"
-        },
-        {
-            type: "input",
-            name: "internId",
-            message: "What's the intern ID?"
-        },
-        {
-            type: "input",
-            name: "internEmail",
-            message: "What's the intern Email?"
-        },
-        {
-            type: "input",
-            name: "internSchool",
-            message: "What is the name of intern's School ?"
-        },
-    ])
-    .then(answers => {
-        // Use user feedback for... whatever!!
-        console.log(answers)
-        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
-        console.log("This is the new engineer" + JSON.stringify(intern))
+        .prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What's the engineer name?"
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What's the engineer ID?"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What's the engineer Email?"
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What's the engineer Github?"
+            },
+        ])
+        .then(answers => {
+            // Use user feedback for... whatever!!
+            console.log(answers)
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
+            console.log("This is the new engineer" + JSON.stringify(engineer))
 
-        employeeArr.push(intern)
-    })
-    .catch(error => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            
-            // Something else when wrong
-        }
+            employeeArr.push(engineer)
+            promptForTeamMember ()
+        })
+        .catch(error => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+
+                // Something else when wrong
+            }
+        });
+}
+
+
+function createIntern() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What's the intern name?"
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "What's the intern ID?"
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What's the intern Email?"
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What is the name of intern's School ?"
+            },
+        ])
+        .then(answers => {
+            // Use user feedback for... whatever!!
+            console.log(answers)
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
+            console.log("This is the new engineer" + JSON.stringify(intern))
+
+            employeeArr.push(intern)
+            promptForTeamMember ()
+        })
+        .catch(error => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+
+                // Something else when wrong
+            }
+        });
+}
+
+
+function createTeam() {
+    fs.writeFile(outputPath, render(employeeArr), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
     });
+}
 
-    function createTeam (){
-        fs.writeFile (outputPath,render(employeeArr), (err) => {
-            if (err) throw err;
-            console.log('The file has been saved!');
-          });
-        }
+createManager()
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
